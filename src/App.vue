bc12c27264ff7df71eb55d122b469686aa1e8d49
andreaconv <andrea2convertini@gmail.com>
2023-06-26 18:53:08 +0200

    [add] chiamata Api

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,32 @@
   <script>
+
+  import { store } from './data/store.js';
+  import axios from 'axios';
+
   export default {
+    name: 'Home',
+
+    data(){
+      return{
+        projects: [],
+      }
+
+    },
+
+    methods: {
+      getApi(){
+        axios.get(store.apiUrl + 'projects')
+              .then(results => {
+                this.projects = results.data;
+                console.log(this.projects)
+              })
+        ;
+      }
+    },
+
+    mounted(){
+      this.getApi();
+    }
   
   }
   </script>
