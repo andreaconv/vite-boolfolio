b9a222544d6107ce3052a96e3338ee0d594f7139
andreaconv <andrea2convertini@gmail.com>
2023-06-27 16:02:27 +0200

    [add] header

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,35 @@
   <script>
 
+  import MyHeader from './partials/MyHeader.vue';
+
+
   export default {
     name: 'App',
+
+    components: {
+      MyHeader
+    },
 
   }
   </script>
 
 <template>
 
-  <router-view></router-view>
+  <div class="body bg-dark text-white">
+
+    <MyHeader />
+  
+    <router-view></router-view>
+
+  </div>
+
   
 </template>
 
 <style lang="scss" scoped>
+
+  .body{
+  min-height: 100vh;
+  }
 
 </style>
---
+++ src/partials/MyHeader.vue
@@ -0,0 +1,42 @@
+<script>
+export default {
+  name: 'MyHeader'
+}
+</script>
+
+<template>
+  <header>
+    <nav class="container py-3">
+      <ul>
+
+        <li>
+          <router-link :to="{ name: 'home' }">Home</router-link>
+        </li>
+
+        <li>
+          <router-link :to="{ name: 'projects' }">Progetti</router-link>
+        </li>
+
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+
+ul{
+  list-style: none;
+  display: flex;
+  li{
+    margin: 0 20px;
+    a{
+      text-decoration: none;
+      color: white;
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+  }
+}
+
+</style>
