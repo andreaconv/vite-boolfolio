6a36efcc85309aae3b1f9e078e35432670ed6e84
andreaconv <andrea2convertini@gmail.com>
2023-06-27 16:54:41 +0200

    [add] page error 404

---
+++ src/pages/Error404.vue
@@ -0,0 +1,17 @@
+  <script>
+  export default {
+    name: 'Error404'
+  }
+  </script>
+  
+<template>
+
+  <div class="container">
+    <h1>Errore 404</h1>
+  </div>
+  
+</template>
+
+<style lang="scss" scoped>
+
+</style>
--- src/router.js
+++ src/router.js
@@ -1,7 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-import Home from './pages/Home.vue'
+import Home     from './pages/Home.vue'
 import Projects from './pages/Projects.vue'
+import Error404 from './pages/Error404.vue'
 
 const router = createRouter({
 
@@ -17,6 +18,13 @@ const router = createRouter({
       path: '/progetti',
       name: 'projects',
       component: Projects
+    },
+
+    // rotta per 404 da mettere in fondo a tutte le rotte 
+
+    {
+      path: '/:pathMatch(.*)*',
+      component: Error404
     }
 
   ]
