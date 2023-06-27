4d30c9efe838620029f2156632775c67a619b6d1
andreaconv <andrea2convertini@gmail.com>
2023-06-27 15:36:11 +0200

    [add] router

---
+++ src/pages/Home.vue
@@ -0,0 +1,14 @@
+  <script>
+  export default {
+    name: 'Home'
+  
+  }
+  </script>
+
+<template>
+  <h1>Home</h1>
+</template>
+
+<style>
+
+</style>
---
+++ src/router.js
@@ -0,0 +1,26 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import Home from './pages/Home.vue'
+import Projects from './pages/Projects.vue'
+
+const router = createRouter({
+
+  history: createWebHistory(),
+
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/progetti',
+      name: 'Projects',
+      component: Projects
+    }
+
+  ]
+
+})
+
+export { router }
